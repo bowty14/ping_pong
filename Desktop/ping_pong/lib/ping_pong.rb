9801454d6a273fb224515143135a079204a29fbc
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-05-04 10:09:02 -0700

    add minimum code to pass test.

--- Desktop/ping_pong/lib/ping_pong.rb
+++ Desktop/ping_pong/lib/ping_pong.rb
@@ -8,6 +8,8 @@
       x = x +1 
       if x % 3 == 0
         array.push('ping')
+      elsif x % 5 == 0
+        array.push('pong')
       else
         array.push(x)
       end
--- Desktop/ping_pong/spec/ping_pong_spec.rb
+++ Desktop/ping_pong/spec/ping_pong_spec.rb
@@ -9,6 +9,8 @@ describe ('#ping_pong') do
   it ("numbers divisible by 3 are replaced with word 'ping'") do
     expect(ping_pong(3)).to(eq([1,2,'ping']))
   end
+  
   it ("numbers divisible by 5 are replaced with word 'pong'") do
     expect(ping_pong(5)).to(eq([1,2,'ping',4,'pong']))
+  end
 end 
