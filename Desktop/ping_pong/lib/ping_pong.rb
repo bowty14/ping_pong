509d1f7140e689296efad8dbb6adaf0a989ab513
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-05-04 09:17:25 -0700

    run test on first spec, expect to fail.

--- Desktop/ping_pong/lib/ping_pong.rb
+++ Desktop/ping_pong/lib/ping_pong.rb
@@ -1,2 +1,6 @@
   #!/usr/bin/env ruby
   require('pry')
+
+  def ping_pong()
+    # put code to pass test
+  end
--- Desktop/ping_pong/spec/ping_pong_spec.rb
+++ Desktop/ping_pong/spec/ping_pong_spec.rb
@@ -1,2 +1,7 @@
 require('rspec')
 require('ping_pong')
+
+describe ('#ping_pong') do 
+ it ("counts from one to a given number") do
+  expect(ping_pong(3)).to(eq([1,2,3]))
+end 
