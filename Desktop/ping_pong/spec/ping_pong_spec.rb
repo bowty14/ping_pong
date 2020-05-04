be8ee884b9a751879396f191cb849082d9994bc5
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-05-04 09:30:02 -0700

    test second spec, expect to fail

--- Desktop/ping_pong/spec/ping_pong_spec.rb
+++ Desktop/ping_pong/spec/ping_pong_spec.rb
@@ -2,7 +2,11 @@ require('rspec')
 require('ping_pong')
 
 describe ('#ping_pong') do 
- it ("counts from one to a given number") do
-  expect(ping_pong(3)).to(eq([1,2,3]))
-  end 
+  it ("counts from one to a given number") do
+    expect(ping_pong(3)).to(eq([1,2,3]))
+  end
+  
+  it ("numbers divisible by 3 are replaced with word ping") do
+    expect(ping_pong(3)).to(eq([1,2,'ping']))
+  end
 end 
